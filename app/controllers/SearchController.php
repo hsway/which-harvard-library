cb23f9b9d1ca724240e7c283d3c08192026b09b4
unknown <hls189@UISWHLPT380900A.university.harvard.edu>
2014-12-10 13:31:02 -0500

    fixed bug when search results are returned from only one library; commented out library code to library name conversion for now

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -27,12 +27,19 @@ class SearchController extends BaseController {
 		$json = file_get_contents($url);
 		$data = json_decode($json, true);
 
+/* Commenting out lib code to lib name conversion for now - only works for arrays, not strings 
+(i.e. when results are returned from >1 library)
+sample search with results from only one library: laravel
+sample search with no results: asdfjkl
+
 		foreach ($data['facets']['facetField']['facet'] as &$item) {
 			$item['term'] = str_replace('GUT', 'Gutman', $item['term']);
 			$item['term'] = str_replace('NET', 'online stuff', $item['term']);
   			$item['term'] = str_replace('WID', 'Widener', $item['term']);
   			$item['term'] = str_replace('WOL', 'Wolbach', $item['term']);
 		}
+
+*/		
 
 		return View::make('results')->with('data', $data);
 
--- app/views/results.blade.php
+++ app/views/results.blade.php
@@ -1,28 +1,59 @@
 @extends('_master')
 
 @section('results')
-<br />
 
-<h2>You should go to <span id="first_result">
+	<br />
 
-<?php $first = reset($data['facets']['facetField']['facet']);
-echo $first['term'];
-?>
+{{--No search results--}}
 
-</span> for that.</h2><br />
+	@if ($data['pagination']['numFound'] == 0)
 
-<p>But here are the full results just in case:</p>
+		<h2>Sorry, no results for that search.</h2>
 
-<div class="row">
-	<div class="col-md-3"><b>Library</b></div>
-	<div class="col-md-3"><b>Hits</b></div>
-</div>
+{{-- Results from multiple libraries --}}
 
-@foreach ($data['facets']['facetField']['facet'] as $item)
-<div class="row">
-	<div class="col-md-3"><?php print $item['term']; ?></div>
-	<div class="col-md-3"><?php print $item['count']; ?></div>
-</div>
-@endforeach
+	@elseif (count($data['facets']['facetField']['facet']) > 2)
+
+		<h2>You should go to <span id="first_result">
+
+		<?php $first = reset($data['facets']['facetField']['facet']);
+		echo $first['term'];
+		?>
+
+		</span> for that.</h2><br />
+
+		<p>But here are the full results just in case:</p>
+
+		<div class="row">
+			<div class="col-md-3"><b>Library</b></div>
+			<div class="col-md-3"><b>Hits</b></div>
+		</div>
+
+		@foreach ($data['facets']['facetField']['facet'] as $item)
+		<div class="row">
+			<div class="col-md-3"><?php print $item['term']; ?></div>
+			<div class="col-md-3"><?php print $item['count']; ?></div>
+		</div>
+		@endforeach
+
+{{-- Results from only one library --}}
+
+	@else
+
+		<h2>You should go to <span id="first_result">{{ $data['facets']['facetField']['facet']['term'] }}</span> for that.</h2><br />
+
+		<p>Full results:</p>
+
+		<div class="row">
+			<div class="col-md-3"><b>Library</b></div>
+			<div class="col-md-3"><b>Hits</b></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-3">{{ $data['facets']['facetField']['facet']['term'] }}</div>
+			<div class="col-md-3">{{ $data['facets']['facetField']['facet']['count'] }}</div>
+		</div>
+
+	@endif
 
 @stop
